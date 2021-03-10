47d6d020737dd7044cc79458b16c92c95d5ffd91
tindrew <tindrewmar@gmail.com>
2021-03-09 23:57:40 -0600

    finished variable exercises

---
+++ String and Variables/src/main/kotlin/Strings.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    println("I like to put a \\ in my code")
+    println("this is a 'c'har")
+    println('c')
+    println('h')
+    println('A')
+    println('r')
+
+}
---
+++ String and Variables/src/main/kotlin/Variables.kt
@@ -0,0 +1,27 @@
+fun main(args: Array<String>) {
+    var tvShows = "many"
+    println(tvShows)
+    tvShows = "too many"
+    println(tvShows)
+
+    val color = "blue" //val are immutable
+    var color2 = "red" //var are normal variables
+    println(color)
+    println(color2)
+
+    val catName = "My cat's name is \"Fluffy\""
+    println(catName)
+
+    var var1 = "Two types of slashes \\ and /."
+    println(var1)
+
+    var url = "https://www.google.com"
+    println(url)
+
+    var bunnyEars = ("(\\ (\\" )
+    println(bunnyEars)
+    var bunnyHead = "(-.-)"
+    println(bunnyHead)
+    var bunnyBody = "o_(\")"
+    println(bunnyBody)
+}
